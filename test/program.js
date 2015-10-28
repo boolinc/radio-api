665f7ddb72c33ed4306dfe40a3654cb5f638479c
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-10-28 03:50:57 -0500

    Add insert and post-insert tests

--- test/program.js
+++ test/program.js
@@ -16,9 +16,18 @@ describe('Programs', function () {
         before(function (done) { db.collection.remove(done); });
 
         it('Programs list must be empty', function () {
-            return db.list().then(function (data) {
-                expect(data).to.have.length(0);
+            return expect(db.list()).to.eventually.have.length(0);
+        });
+
+        it('Creates a new program', function () {
+            return db.insert({
+                _id: 'tripping',
+                name: 'The Midnight Tripping Show'
             });
+        });
+
+        it('Programs list should contain one element', function () {
+            return expect(db.list()).to.eventually.have.length(1);
         });
 
     });
