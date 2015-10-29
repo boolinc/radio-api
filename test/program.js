47bec46c3f8b5724d026bf751c5591861c337b66
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-10-29 02:19:18 -0500

    Tests for Insert

--- test/program.js
+++ test/program.js
@@ -2,34 +2,74 @@
 'use strict';
 
 describe('Programs', function () {
-    var booljs  = require('bool.js')
-    ,   db, dao;
+    var booljs      = require('bool.js')
+    ,   supertest   = require('supertest-as-promised')
+    ,   db, dao, agent;
 
     before(function () {
         return booljs('com.gentebool.radio').run().then(function (api) {
             db = new api.app.models.Program();
             dao = new api.app.dao.Program();
+            agent = supertest(api.server);
         });
     });
 
     describe('Database', function () {
-        before(function (done) { db.collection.remove(done); });
+        before((done) => { db.collection.remove(done); });
 
-        it('Programs list must be empty', function () {
+        it('Programs list must be empty', () => {
             return expect(db.list()).to.eventually.have.length(0);
         });
 
-        it('Creates a new program', function () {
+        it('Creates a new program', () => {
             return db.insert({
                 _id: 'tripping',
                 name: 'The Midnight Tripping Show'
             });
         });
 
-        it('Programs list should contain one element', function () {
+        it('Programs list should contain one element', () => {
             return expect(db.list()).to.eventually.have.length(1);
         });
 
     });
+
+    describe('DAO', function () {
+        before((done) => { db.collection.remove(done); });
+
+        it('Creates a new program', () => {
+            return dao.insert({
+                _id: 'tripping',
+                name: 'The Midnight Tripping Show'
+            });
+        });
+    });
+
+    describe('Controller', function () {
+        before((done) => { db.collection.remove(done); });
+
+        it('Creates a new program', () => {
+            return (agent
+                .post('/api/programs')
+                .send({
+                    _id: 'tripping',
+                    name: 'The Midnight Tripping Show'
+                })
+                .expect(200)
+            );
+        });
+
+        it('If data is missing, retrieve a 400', () => {
+            return (agent
+                .post('/api/programs')
+                .send({
+                    name: 'The Midnight Tripping Show'
+                })
+                .expect(400)
+            );
+        });
+    });
+
+
 
 });
